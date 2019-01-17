e680f60757c8645c1e5e9a53ef5511711ce7a3bc
Jeroen De Dauw <jeroendedauw@gmail.com>
2019-01-17 11:08:06 +0100

    Add factory test

---
+++ tests/Unit/FactoryTest.php
@@ -0,0 +1,42 @@
+<?php
+
+declare( strict_types = 1 );
+
+namespace FileFetcher\Cache\PackagePrivate;
+
+use FileFetcher\Cache\Factory;
+use FileFetcher\NullFileFetcher;
+use PHPUnit\Framework\TestCase;
+use Psr\SimpleCache\CacheInterface;
+
+/**
+ * @covers \FileFetcher\Cache\Factory
+ *
+ * @licence BSD-3-Clause
+ * @author Jeroen De Dauw < jeroendedauw@gmail.com >
+ */
+class FactoryTest extends TestCase {
+
+	public function testNewCachingFetcherReturnsFetcherThatUsesCache() {
+		$cache = ( new Factory() )->newCachingFetcher(
+			new NullFileFetcher(),
+			$this->newStubCache( '42' ),
+			60
+		);
+
+		$this->assertSame(
+			'42',
+			$cache->fetchFile( 'whatever' )
+		);
+	}
+
+	private function newStubCache( string $stubValue ): CacheInterface {
+		$cache = $this->createMock( CacheInterface::class );
+
+		$cache->method( 'get' )
+			->willReturn( $stubValue );
+
+		return $cache;
+	}
+
+}
